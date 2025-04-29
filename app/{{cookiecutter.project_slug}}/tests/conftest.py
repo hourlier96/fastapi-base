05e83296d06de1fae1b3a73d58afecdbc98f0e28
Augustin <augustin.hourlier@devoteamgcloud.com>
2025-04-29 13:37:18 +0200

    Update CI, add test client

--- app/{{cookiecutter.project_slug}}/tests/conftest.py
+++ app/{{cookiecutter.project_slug}}/tests/conftest.py
@@ -1,10 +1,13 @@
 from typing import AsyncGenerator
 
 import pytest_asyncio
-from httpx import AsyncClient
+from httpx import ASGITransport, AsyncClient
+
+from app.main import app as actual_app
 
 
 @pytest_asyncio.fixture(scope="function")
 async def client() -> AsyncGenerator[AsyncClient, None]:
-    async with AsyncClient(base_url="http://localhost:8000") as ac:
+    transport = ASGITransport(app=actual_app)
+    async with AsyncClient(transport=transport, base_url="http://fakeurl") as ac:
         yield ac
